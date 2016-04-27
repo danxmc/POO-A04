236558345e5567a519d3980d2418c39c21b96046
danx_ <danx_@DESKTOP>
2016-04-27 16:56:34 -0500

    Modified Object Type in each Object

--- src/poo/a05/POOA05.java
+++ src/poo/a05/POOA05.java
@@ -5,6 +5,9 @@
  */
 package poo.a05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author danx_
@@ -16,7 +19,7 @@ public class POOA05 {
      */
     public static void main(String[] args) {
         //Donde T son Integers
-        GenericClass c1 = new GenericClass();
+        GenericClass<Integer> c1 = new GenericClass<Integer>();
         c1.almacenarUltimoDisponible(100);
         c1.almacenarUltimoDisponible(36);
         c1.almacenarUltimoDisponible(1023);
@@ -24,7 +27,7 @@ public class POOA05 {
         System.out.println("");
         
         //Donde T son Strings
-        GenericClass c2 = new GenericClass();
+        GenericClass<String> c2 = new GenericClass<String>();
         c2.almacenarUltimoDisponible("Hello");
         c2.almacenarUltimoDisponible("dudududu");
         c2.almacenarUltimoDisponible("OC");
@@ -32,27 +35,24 @@ public class POOA05 {
         System.out.println("");
         
         //Donde T son Floats
-        GenericClass c3 = new GenericClass();
-        c3.almacenarUltimoDisponible(2.1234);
-        c3.almacenarUltimoDisponible(1.0000);
-        c3.almacenarUltimoDisponible(3.1416);
+        GenericClass<Float> c3 = new GenericClass<Float>();
+        c3.almacenarUltimoDisponible(2.1234f);
+        c3.almacenarUltimoDisponible(1.0000f);
+        c3.almacenarUltimoDisponible(3.1416f);
         System.out.println(c3.devuelveAlmacenados());
         System.out.println("");
         
         //Donde T son Personas
-        GenericClass c4 = new GenericClass();
+        GenericClass<Persona> c4 = new GenericClass<Persona>();
         Persona p1 = new Persona("Neto", "m", 69);
         Persona p2 = new Persona("Luisa", "f", 18);
         c4.almacenarUltimoDisponible(p1);
         c4.almacenarUltimoDisponible(p2);
-        Persona x = (Persona) c4.devuelveAlmacenados().get(0);
-        System.out.println(x.getNombre());
-        System.out.println(x.getSexo());
-        System.out.println(x.getEdad());
-        Persona x1 = (Persona) c4.devuelveAlmacenados().get(1);
-        System.out.println(x1.getNombre());
-        System.out.println(x1.getSexo());
-        System.out.println(x1.getEdad());
+        List<Persona> t = new ArrayList();
+        t = c4.devuelveAlmacenados();
+        for (int i = 0; i < t.size(); i++) {
+            System.out.println(t.get(i).getNombre() + ", " + t.get(i).getSexo() + ", " + t.get(i).getEdad());
+        }
         System.out.println("");
     }
     
